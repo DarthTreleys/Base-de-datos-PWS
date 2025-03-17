f641cf6b0e43848358ce2d2e85899fb46754424d
Treleys <joancosdo@gmail.com>
2025-03-17 17:32:55 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -4,6 +4,6 @@ if ($_SESSION['role'] !== "admin") {
     header("Location: login.php");
     exit();
 }
-echo "<h1>Benvingut Admin – Cognom i Cognom</h1>";
+echo "<h1>Benvingut Admin – Marc Jimenez i Joan Cos</h1>";
 echo '<a href="logout.php">Logout</a>';
 ?>
